da3be4e746e3d54ab5aa7b232c74a54b3d247e51
hriteek <hriteekbista@gmail.com>
2019-01-17 14:14:05 +0545

    browserrouter changed to hashroter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Contact from "./components/Contact/Contacts";
 import Header from "./components/Layout/Header";
